a62a626d11c92a920864721c21dc5b3b5b3376aa
Bryan Bibb <bdbibb@gmail.com>
2024-01-29 10:56:57 -0500

    PHP labs landing page

--- PHP/htdocs/index.php
+++ PHP/htdocs/index.php
@@ -13,14 +13,16 @@
     </head>
     <body>
         <!-- Responsive navbar-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
             <div class="container px-5">
                 <a class="navbar-brand" href="#!">Bryan Bibb</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="https://github.com/bbibb">Github</a></li>
+                        <li class="nav-item"><a class="nav-link" aria-current="page" href="#!">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" aria-current="page" href="#lab_files">Labs</a></li>
+                        <li class="nav-item"><a class="nav-link" aria-current="page" href="#project_files">Final Project</a></li>
+                        <li class="nav-item"><a class="nav-link" target="_blank" href="https://github.com/bbibb">Github</a></li>
                     </ul>
                 </div>
             </div>
@@ -29,7 +31,7 @@
         <div class="container px-4 px-lg-5">
             <!-- Heading Row-->
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
-                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="assets/bryan_header.jpeg" alt="..." /></div>
+                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="assets/water_header.jpeg" alt="..." /></div>
                 <div class="col-lg-5">
                     <h1 class="font-weight-light">PHP Programming</h1>
                     <p>This is my portfolio/landing page for this semester's CPT283 course at Greenville Technical College in Greenville, SC. </p>
@@ -38,7 +40,7 @@
             </div>
             <!-- Call to Action-->
             <div class="card text-white bg-secondary my-5 py-4 text-center">
-                <div class="card-body"><p class="text-white m-0"><h3>Links to the weekly Lab assignments</h3></p></div>
+                <div class="card-body" id="lab_files"><p class="text-white m-0"><h3>Links to the weekly Lab assignments</h3></p></div>
             </div>
             <!-- Content Row-->
             <div class="row gx-4 gx-lg-5">
@@ -46,32 +48,61 @@
                     <div class="card h-100">
                         <div class="card-body">
                             <h2 class="card-title">Exercise 2-1</h2>
-                            <p class="card-text">Build the Product Discount application. This lab introduces $_POST arrays to receive and work with user input data.</p>
+                            <p class="card-text"><strong>Build the Product Discount application.</strong> This lab introduces $_POST arrays to receive and work with user input data.</p>
                         </div>
-                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">Launch</a></div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" target="_blank" href="labs/ch02_ex1/index.html">Launch</a></div>
                     </div>
                 </div>
                 <div class="col-md-4 mb-5">
                     <div class="card h-100">
                         <div class="card-body">
                             <h2 class="card-title">Exercise 2-2</h2>
-                            <p class="card-text">Enhance the Future Value application. This lab introduces input data validation, error messages, and the PHP date() function.</p>
+                            <p class="card-text"><strong>Enhance the Future Value application.</strong> This lab introduces input data validation, error messages, and the PHP date() function.</p>
                         </div>
-                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">Launch</a></div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" target="_blank" href="labs/ch02_ex2/index.php">Launch</a></div>
                     </div>
                 </div>
                 <div class="col-md-4 mb-5">
                     <div class="card h-100">
                         <div class="card-body">
-                            <h2 class="card-title">Exercise 3-1</h2>
-                            <p class="card-text">Use PHPMyAdmin with a database. This lab imports the course's database scripts into the MySQL database and introduces SELECT, UPDATE, and DELETE statements.</p>
+                            <h2 class="card-title">Exercise 4-1</h2>
+                            <p class="card-text"><strong>Enhance the Product Manager Application.</strong> This lab introduces the process of connecting SQL database management with the PHP backed and HTML webpage code.</p>
                         </div>
-                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">Launch</a></div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" target="_blank" href="labs/ch04_ex1/index.php">Launch</a></div>
                     </div>
                 </div>
             </div>
+            <div class="row gx-4 gx-lg-5">
+            <div class="col-md-4 mb-5">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h2 class="card-title">Exercise 5-1</h2>
+                        <p class="card-text"><strong>Enhance the Guitar Shop application.</strong> This lab introduces the MVC pattern to organize code. </p>
+                    </div>
+                    <div class="card-footer"><a class="btn btn-primary btn-sm" target="_blank" href="labs/ch05_ex1/index.php">Launch</a></div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-5">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h2 class="card-title">Exercise 6-1</h2>
+                        <p class="card-text"><strong>Trace with echo statements.</strong> This lab practices using 'echo' statements to trace the execution of code.</p>
+                    </div>
+                    <div class="card-footer"><a class="btn btn-primary btn-sm" target="_blank" href="labs/ch06_ex1/index.php">Launch</a></div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-5">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h2 class="card-title">Exercise 7-1</h2>
+                        <p class="card-text"><strong>Get input and display output.</strong> This lab uses forms with a variety of controls to get and display data.</p>
+                    </div>
+                    <div class="card-footer"><a class="btn btn-primary btn-sm" target="_blank" href="labs/ch07_ex1/index.php">Launch</a></div>
+                </div>
+            </div>
+        </div>
             <div class="card text-white bg-secondary my-5 py-4 text-center">
-                <div class="card-body"><p class="text-white m-0"><h3>Links to Final Project pages.</h3></p></div>
+                <div class="card-body" id="project_files"><p class="text-white m-0"><h3>Links to Final Project pages.</h3></p></div>
             </div>
         </div>
         <!-- Footer-->
